935d604331151e70bab698b49dd14309c63b854e
Matias Gallardo <matias.jgallardo@gmail.com>
2023-02-13 16:14:21 -0300

    desafio completo

--- index.js
+++ index.js
@@ -25,6 +25,25 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector("ul.lista");
+
+  let n = listaEl.childElementCount;
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+    listaEl.removeChild(listaEl.querySelector("li"));
+  }
+
+  const arrayNewLiEls = [];
+  cosasQueAprendimos.forEach((e, i) => {
+    arrayNewLiEls.push(document.createElement("li"));
+    console.log(e.tema);
+    arrayNewLiEls[i].textContent = e.tema;
+    if (!e.class == "") {
+      arrayNewLiEls[i].classList = e.class;
+    }
+    listaEl.appendChild(arrayNewLiEls[i]);
+  });
+}
 
 main();
