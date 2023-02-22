179206dd3fadcd51f1cb3e7038d9b34aa854437e
Matias Gallardo <matias.jgallardo@gmail.com>
2023-02-22 13:10:02 -0300

    Metodos alternativos

--- index.js
+++ index.js
@@ -25,25 +25,72 @@ const cosasQueAprendimos = [
   },
 ];
 
+// Metodo inicial mio
+
+// function main() {
+//   const listaEl = document.querySelector("ul.lista");
+
+//   let n = listaEl.childElementCount;
+//   for (let i = 0; i < n; i++) {
+//     console.log(i);
+//     listaEl.removeChild(listaEl.querySelector("li"));
+//   }
+
+//   const arrayNewLiEls = [];
+//   cosasQueAprendimos.forEach((e, i) => {
+//     arrayNewLiEls.push(document.createElement("li"));
+//     console.log(e.tema);
+//     arrayNewLiEls[i].textContent = e.tema;
+//     if (!e.class == "") {
+//       arrayNewLiEls[i].classList = e.class;
+//     }
+//     listaEl.appendChild(arrayNewLiEls[i]);
+//   });
+// }
+
+// main();
+
+// Metodo alternativo 1 (.append() y .innerHTML())
+
+// function createLi(text, elClass) {
+//   const newLiEl = document.createElement("li");
+//   newLiEl.append(text);
+//   if (elClass !== "") newLiEl.classList = elClass;
+//   return newLiEl;
+// }
+
+// function listItems(items) {
+//   return [...Array(items.length)].map((e, i) =>
+//     createLi(items[i].tema, items[i].class)
+//   );
+// }
+
+// function main() {
+//   const listaEl = document.querySelector("ul.lista");
+//   listaEl.innerHTML = "";
+//   listaEl.append(...listItems(cosasQueAprendimos));
+// }
+
+// main();
+
+// Metodo alternativo 2 (.append() y .innerHTML())
+
+function createLi(text, elClass) {
+  const newLiEl = document.createElement("li");
+  newLiEl.append(text);
+  if (elClass !== "") newLiEl.classList = elClass;
+  return newLiEl;
+}
+
+function listItems(items) {
+  return [...Array(items.length)].map((e, i) =>
+    createLi(items[i].tema, items[i].class)
+  );
+}
+
 function main() {
   const listaEl = document.querySelector("ul.lista");
-
-  let n = listaEl.childElementCount;
-  for (let i = 0; i < n; i++) {
-    console.log(i);
-    listaEl.removeChild(listaEl.querySelector("li"));
-  }
-
-  const arrayNewLiEls = [];
-  cosasQueAprendimos.forEach((e, i) => {
-    arrayNewLiEls.push(document.createElement("li"));
-    console.log(e.tema);
-    arrayNewLiEls[i].textContent = e.tema;
-    if (!e.class == "") {
-      arrayNewLiEls[i].classList = e.class;
-    }
-    listaEl.appendChild(arrayNewLiEls[i]);
-  });
+  listaEl.replaceChildren(...listItems(cosasQueAprendimos));
 }
 
 main();
